619193434b0f5ed1a91e82af8833bdcbff31409d
Carlos Venturelli <carlos.venturelli@hotmail.com>
2023-08-25 14:32:41 -0300

    Added challenge headers

--- src/Section10/Section10Challenge2.cpp
+++ src/Section10/Section10Challenge2.cpp
@@ -1,3 +1,90 @@
+/*
+Section 10 Challenge 2
+======================
+
+Write a C++ program that displays a Letter Pyramid from a user-provided std::string.
+
+Prompt the user to enter a std::string and then from that string display a Letter Pyramid as follows:
+
+It's much easier to understand the Letter Pyramid given examples.
+
+If the user enters the string "ABC", then your program should display:
+
+  A
+ ABA
+ABCBA
+
+
+If the user enters the string, "12345", then your program should display:
+
+    1
+   121
+  12321
+ 1234321
+123454321
+
+If the user enters 'ABCDEFG', then your program should display:
+
+      A
+     ABA
+    ABCBA
+   ABCDCBA
+  ABCDEDCBA
+ ABCDEFEDCBA
+ABCDEFGFEDCBA
+
+If the user enters 'ABCDEFGHIJKLMNOPQRSTUVWXYZ', then your program should display:
+
+                         A
+                        ABA
+                       ABCBA
+                      ABCDCBA
+                     ABCDEDCBA
+                    ABCDEFEDCBA
+                   ABCDEFGFEDCBA
+                  ABCDEFGHGFEDCBA
+                 ABCDEFGHIHGFEDCBA
+                ABCDEFGHIJIHGFEDCBA
+               ABCDEFGHIJKJIHGFEDCBA
+              ABCDEFGHIJKLKJIHGFEDCBA
+             ABCDEFGHIJKLMLKJIHGFEDCBA
+            ABCDEFGHIJKLMNMLKJIHGFEDCBA
+           ABCDEFGHIJKLMNONMLKJIHGFEDCBA
+          ABCDEFGHIJKLMNOPONMLKJIHGFEDCBA
+         ABCDEFGHIJKLMNOPQPONMLKJIHGFEDCBA
+        ABCDEFGHIJKLMNOPQRQPONMLKJIHGFEDCBA
+       ABCDEFGHIJKLMNOPQRSRQPONMLKJIHGFEDCBA
+      ABCDEFGHIJKLMNOPQRSTSRQPONMLKJIHGFEDCBA
+     ABCDEFGHIJKLMNOPQRSTUTSRQPONMLKJIHGFEDCBA
+    ABCDEFGHIJKLMNOPQRSTUVUTSRQPONMLKJIHGFEDCBA
+   ABCDEFGHIJKLMNOPQRSTUVWVUTSRQPONMLKJIHGFEDCBA
+  ABCDEFGHIJKLMNOPQRSTUVWXWVUTSRQPONMLKJIHGFEDCBA
+ ABCDEFGHIJKLMNOPQRSTUVWXYXWVUTSRQPONMLKJIHGFEDCBA
+ABCDEFGHIJKLMNOPQRSTUVWXYZYXWVUTSRQPONMLKJIHGFEDCBA
+
+If the user enters "C++isFun!", then your program should display:
+
+        C
+       C+C
+      C+++C
+     C++i++C
+    C++isi++C
+   C++isFsi++C
+  C++isFuFsi++C
+ C++isFunuFsi++C
+C++isFun!nuFsi++C
+
+Think about the problem before you begin and break it down into steps.
+
+This can be a very challenging problem to solve!
+
+*****  Don't forget to paste your code solution when you submit.******
+
+*****  Also, submit it so your peers can see it and possibly comment. ******
+
+Have fun!
+*/
+
 #include <iostream>
 #include <string>
 
--- src/Section6/Section6Challenge.cpp
+++ src/Section6/Section6Challenge.cpp
@@ -1,3 +1,29 @@
+/*
+Section 6 Challenge
+===================
+
+Frank's Carpet Cleaning Service
+Charges:
+  $25 per small room
+  $35 per large room
+Sales tax rate is 6%
+Estimates are valid for 30 days
+
+Prompt the user for the number of small and large rooms they would like cleaned
+and provide an estimate such as:
+
+Estimate for carpet cleaning service
+Number of small rooms: 3
+Number of large rooms: 1
+Price per small room: $25
+Price per large room: $35
+Cost : $110
+Tax: $6.6
+===============================
+Total estimate: $116.6
+This estimate is valid for 30 days
+*/
+
 #include <iostream>
 
 using std::cin;
@@ -11,7 +37,7 @@ int main()
   const double SALES_TAX_RATE = 0.06;
   const int ESTIMATE_VALID = 30; // Days
 
-  cout << "Hello! Welcome to Carlos' Carpet Cleaning Service" << endl << endl;
+  cout << "Hello! Welcome to Frank's Carpet Cleaning Service" << endl << endl;
 
   cout << "How many small rooms would you like cleaned? ";
   int smallRooms {0};
